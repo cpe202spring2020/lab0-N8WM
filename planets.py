935735f19b4802f4b5a920350f217ea583fd40c8
Nathan McCutchen <n8wm@icloud.com>
2020-04-06 14:02:06 -0700

    Committing work

--- planets.py
+++ planets.py
@@ -1,7 +1,12 @@
 def weight_on_planets():
-   # write your code here
+   eWeight = int(input("What do you weigh on earth? "))
+   mWeight = eWeight * 0.38
+   jWeight = eWeight * 2.34
+   
+   print("\nOn Mars you would weigh", mWeight,"pounds.\nOn Jupiter you would weigh", jWeight, "pounds.")
+   
    
    
    
 if __name__ == '__main__':
-   weight_on_planets()
+    weight_on_planets()
--- planets_tests.py
+++ planets_tests.py
@@ -6,7 +6,7 @@ class Test_planets(unittest.TestCase):
 
     def test_01(self):
         sys.stdin = io.StringIO("136")
-        sys.stdout = student_output = io.StringIO()        
+        sys.stdout = student_output = io.StringIO()
         expected_out = "What do you weigh on earth? \nOn Mars you would weigh 51.68 pounds.\nOn Jupiter you would weigh 318.24 pounds."
         weight_on_planets()
         #print(student_output.getvalue().strip())
